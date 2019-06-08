904cdc4a2bf73a52a566ad6e119043e3b3e97003
Daiki-Ishida <missoula0132@gmail.com>
2019-06-08 03:22:41 +0000

    Create Prodect model

--- db/schema.rb
+++ db/schema.rb
@@ -10,26 +10,48 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2019_06_08_031543) do
 
-ActiveRecord::Schema.define(version: 2019_06_06_112253) do
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "disks", force: :cascade do |t|
+    t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "orders", force: :cascade do |t|
     t.string "payment_methods"
     t.string "shipment_status"
     t.string "destination"
     t.integer "user_id"
-
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "products", force: :cascade do |t|
+    t.integer "genre_id"
+    t.integer "artist_id"
+    t.integer "label_id"
+    t.string "product_name"
+    t.string "jacket_image"
+    t.integer "price"
+    t.text "description"
+    t.integer "stock"
+    t.boolean "recommend"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "reviews", force: :cascade do |t|
     t.integer "user_id"
     t.integer "product_id"
     t.integer "star"
     t.text "body"
-
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
