3e381323519372d9bd7bb2f69b955eb8933cebee
Daiki-Ishida <missoula0132@gmail.com>
2019-06-08 06:27:26 +0000

    hoge

---
+++ app/controllers/ad_products_controller.rb
@@ -0,0 +1,25 @@
+class AdProductsController < ApplicationController
+  def new
+    @product = Product.new
+  end
+
+  def create
+
+  end
+
+  def index
+    
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def show
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :ad_products, only:[:new, :create, :index, :show, :edit, :update]
 end
