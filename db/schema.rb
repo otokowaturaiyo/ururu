5da3f5f781393fce78fd20bdcedef45aada850da
playboy9n <playboy9n@gmail.com>
2019-06-08 03:36:59 +0000

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+
 ActiveRecord::Schema.define(version: 2019_06_08_031543) do
+
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
@@ -32,6 +34,7 @@ ActiveRecord::Schema.define(version: 2019_06_08_031543) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 
   create_table "products", force: :cascade do |t|
     t.integer "genre_id"
