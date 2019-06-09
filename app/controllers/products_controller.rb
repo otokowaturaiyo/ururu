a4e5ba5cc5c524f39b0053476c444f3f830f768f
toraperu <toraperu@icloud.com>
2019-06-09 05:28:45 +0000

    一覧表示-interim

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,9 +2,7 @@ class ProductsController < ApplicationController
   PER = 6
 
   def index
-  	@products = Product.page(params[:page]).per(PER)
-  	@product = Product.find(params[:id])
-  	@artists = artists.where(artist_id: product_id.artist.id)
+  	@products = Product.all
   end
 
   def show
