9665eefc6d4960d01d352f122a0400f61a1deaa6
toraperu <toraperu@icloud.com>
2019-06-09 02:55:29 +0000

    plus icon

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,10 @@
 class ProductsController < ApplicationController
+  PER = 6
+
   def index
+  	@products = Product.page(params[:page]).per(PER)
+  	@product = Product.find(params[:id])
+  	@artists = artists.where(artist_id: product_id.artist.id)
   end
 
   def show
