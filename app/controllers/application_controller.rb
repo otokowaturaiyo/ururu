e8a8ecc76631d8d1a0b5320bc6d7808d72b68212
kyotarooooo <i.kentaro-0916@outlook.com>
2019-06-09 13:33:37 +0900

    カラム作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+	protected
+
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(sign_up, keys: [:kanji_lastname, :kanji_firstname, :kana_lastname, :kana_firstname, :user_name, :postal_code, :postal_address, :phone_number])
 end
