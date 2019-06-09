4d92d08fee417da4c26556ef99baf7683ab2a06e
playboy9n <playboy9n@gmail.com>
2019-06-09 06:26:12 +0000

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,14 @@ class ApplicationController < ActionController::Base
 
 	def configure_permitted_parameters
 		devise_parameter_sanitizer.permit(sign_up, keys: [:kanji_lastname, :kanji_firstname, :kana_lastname, :kana_firstname, :user_name, :postal_code, :postal_address, :phone_number])
-  end
+  	end
 	def after_sign_in_path_for(resource)
-	if resouce.is_a?(AdminUser)
-		admins_top_path
-	else
-		users_root_path
+		if resource.is_a?(Admin)
+			admins_top_path
+		else
+			users_root_path
+		end
 	end
-end
 	# def after_sign_out_path_for(resource)
 	# end
 
