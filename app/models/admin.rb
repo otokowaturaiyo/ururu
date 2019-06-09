ea296b9ec47cfd5de0cb7003d81bf03ac9b815f0
playboy9n <playboy9n@gmail.com>
2019-06-09 12:31:24 +0000

    add validation

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,4 +2,6 @@ class Admin < ApplicationRecord
 	devise :database_authenticatable, :registerable,
 	       :recoverable, :rememberable, :validatable
 	attachment :profile_image_url
+	validates :encrypted_password, length: { minimum: 6 }
+	validates :email, uniqueness: true
 end
