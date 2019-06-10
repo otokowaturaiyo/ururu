983f33261527ad43a7ca2e1aae2ed43ba2132bb8
toraperu <toraperu@icloud.com>
2019-06-10 12:31:18 +0000

    収録曲表完成

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
 	belongs_to :genre
 	belongs_to :artist
 	belongs_to :label
-	#has_many :disks, dependent: :destroy
+	has_many :disks, dependent: :destroy
 
 	validates :product_name, uniqueness: true
 
