0a85d81153b87fdcfa835bafc4572f814e5faa89
kyotarooooo <i.kentaro-0916@outlook.com>
2019-06-11 15:59:53 +0900

    エンドユーザー（未：レイアウト、退会機能）

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
+	attachment :profile_image_url
 	# validates :title, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :kanji_lastname, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :kanji_firstname, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :kana_lastname, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :kana_lastname, format: { with: /\A[ァ-ヶー－]+\z/ }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_08_115629) do
+ActiveRecord::Schema.define(version: 2019_06_10_050529) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -99,9 +99,9 @@ ActiveRecord::Schema.define(version: 2019_06_08_115629) do
     t.string "postal_code", null: false
     t.string "postal_address", null: false
     t.string "phone_number", null: false
-    t.text "profile_image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "profile_image_url_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
