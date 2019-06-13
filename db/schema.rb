41504579b7ff040d23b4fae40f8751f044eabefc
playboy9n <playboy9n@gmail.com>
2019-06-13 11:44:49 +0000

    ダブりカラムの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_06_13_054758) do
-
+ActiveRecord::Schema.define(version: 2019_06_11_093524) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,6 +18,11 @@ ActiveRecord::Schema.define(version: 2019_06_13_054758) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
@@ -128,13 +131,12 @@ ActiveRecord::Schema.define(version: 2019_06_13_054758) do
     t.string "postal_code", null: false
     t.string "postal_address", null: false
     t.string "phone_number", null: false
+    t.text "profile_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "resignation"
+    t.boolean "resignation", default: false, null: false
     t.datetime "resigned_at"
     t.text "profile_image_url_id"
-    t.boolean "resignation"
-    t.datetime "resigned_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
