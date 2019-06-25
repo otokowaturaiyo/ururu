453376483548fe9322ad995957582c456b664336
Daiki-Ishida <missoula0132@gmail.com>
2019-06-25 10:02:11 +0000

    modify callback / admins-orders

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,7 +1,7 @@
 class Admin < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  attachment :profile_image_url
+  attachment :profile_image
   validates :encrypted_password, length: { minimum: 6 }
   validates :email, uniqueness: true
 end
