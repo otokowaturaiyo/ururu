8762806c7761bc7d6aaf00a98d6469d9eb795795
Daiki-Ishida <missoula0132@gmail.com>
2019-06-25 07:13:02 +0000

    add test code

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -6,26 +6,43 @@ RSpec.describe UsersController, type: :controller do
       it 'ログインページにリダイレクトされるか' do
         get :show, params: {id: '1'}
         expect(response).to redirect_to new_user_session_path
-        expect(response.body).to include("flashの中身・・・")
+        # expect(response.body).to include("flashの中身・・・")
       end
     end
     context 'ログインしている場合' do
       before do
-        @user = User.create
+        @user1 = FactoryBot.create(:user)
+        @user2 = FactoryBot.create(:user)
+        sign_in @user1
       end
       context '自分のマイページに行く場合' do
-        it "正しく表示されるか" do
+        before do
           get :show, params: {id: '1'}
+        end
+        it "正しく表示されるか" do
+          expect(response.status).to eq 200
         end
       end
       context '他人のマイページに行く場合' do
         it "flashが表示されるか" do
           get :show, params: {id: '2'}
-          expect(response.body).to include("権限がありません。")
+          expect(response).to redirect_to user_path(@user1)
+          expect(flash[:warning]).to include("権限がありません。")
         end
       end
     end
   end
+
+  describe 'ユーザー編集' do
+    context 'ログインしていない場合' do
+      it 'ログインページにリダイレクトされるか' do
+        get :edit, params: {id: '1'}
+        expect(response).to redirect_to new_user_session_path
+        # expect(response.body).to include("flashの中身・・・")
+      end
+    end
+  end
+
 
 
 end
