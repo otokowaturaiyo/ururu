a0015d70624256bcab4adff43f0db528d0561ae6
playboy9n <playboy9n@gmail.com>
2019-06-26 04:48:26 +0000

    rspec練習

--- spec/controllers/likes_controller_spec.rb
+++ spec/controllers/likes_controller_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
-RSpec.describeLikesController, type: :controller do
-
+RSpec.describe LikesController, type: :controller do
+  before do
+    @products1 = FactoryBot.create(:product)
+    @user1 = FactoryBot.create(:user)
+      @user2 = FactoryBot.create(:user)
+    sign_in @user1
+  end
+  describe '商品の詳細ページ' do
+    before do
+      get :show, params: {id: '1'}
+    end
+    context 'ログインしていない場合' do
+      context '押せないようになっている' do
+        visit product_path(@products1)
+        expect(page).to_not have_button 'Save'
+      end
+    end
+  end
+    context 'ログインしている場合' do
+      before do
+        @products1 = FactoryBot.create(:product)
+      end
+        context 'いいねを押せる' do
+          click_buttun 'save'
+      end
+        context 'いいねを外せる' do
+         visit  product_path(@products1)
+         click_buttun 'destroy'
+      end
+    end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -65,14 +65,46 @@ RSpec.describe UsersController, type: :controller do
     end
   end
 
-  describe 'ユーザーのアップデート' do
-    context 'ユーザーのアップデートができているか'
+  describe '自分の情報を更新' do
     before do
-    pacth :update, params: {id: '1'}
+      @user1 = FactoryBot.create(:user)
+      @user2 = FactoryBot.create(:user)
+      sign_in @user1
+    end
+      context '自分の情報を更新できている場合'
+      before do
+      pacth :update, params: {id: '1'}
+      end
+        it 'マイページにリダイレクトされているか' do
+          get :show, params: {id: '1'}
+        end
+        it 'flashが表示されるか' do
+          expect(flash[:notice]).to include("編集内容を更新しました！")
+        end
+      context '自分の情報更新に失敗した場合'
+        it 'そのままeditページにとどまっているか' do
+          expect(response.status).to eq 200
+        end
+      end
   end
-
-
-  end
+  describe 'ユーザーが退会する場合' do
+    before do
+      get resign_path
+    end
+      it '正しく表示されるか' do
+        expect(response.status).to eq 200
+      end
+    context 'やっぱり退会をやめた場合' do
+      it '自分の編集ページへ戻っているか' do
+        expect(response).to redirect_to user_path(@user1)
+      end
+    end
+    context '退会を決意した場合' do
+      it 'flashが表示されるか' do
+        expect(response).to redirect_to root_path
+        expect(flash[:danger]).to include("退会しました。")
+      end
+    end
 
 
 
